ea8eef11835a06c24030617fd9c08d988b99fc00
ASIBUKA Group <asibukagroup@gmail.com>
2025-04-03 04:00:44 +0700

    edit floating menu

--- _includes/main.js
+++ _includes/main.js
@@ -1,6 +1,6 @@
 //Dark Mode
 document.addEventListener("DOMContentLoaded",function(){const e=document.getElementById("dark-mode-toggle"),t=document.body;"enabled"===localStorage.getItem("dark-mode")&&(t.classList.add("dark-mode"),e.textContent="☀️"),e.addEventListener("click",function(){t.classList.toggle("dark-mode"),t.classList.contains("dark-mode")?(localStorage.setItem("dark-mode","enabled"),e.textContent="☀️"):(localStorage.setItem("dark-mode","disabled"),e.textContent="🌙")})});
-// Floating
+// Floating Menu
 let lastScrollTop=0;const navbar=document.querySelector(".floating-navbar");window.addEventListener("scroll",function(){var a=window.pageYOffset||document.documentElement.scrollTop;a>lastScrollTop?(navbar.style.transform="translate(-50%, -100%)",navbar.style.opacity="0"):(navbar.style.transform="translate(-50%, 0)",navbar.style.opacity="1"),lastScrollTop=a});
 //Sidebar
 const hamburger=document.getElementById("hamburger-menu"),sidebar=document.getElementById("sidebar"),closeSidebar=document.getElementById("close-sidebar"),overlay=document.getElementById("overlay");hamburger.addEventListener("click",()=>{sidebar.classList.add("open"),overlay.classList.add("show")}),closeSidebar.addEventListener("click",()=>{sidebar.classList.remove("open"),overlay.classList.remove("show")}),overlay.addEventListener("click",()=>{sidebar.classList.remove("open"),overlay.classList.remove("show")});
