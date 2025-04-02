5d33669bc2d1c61777b53fb917639b91c3f9d800
ASIBUKA Group <asibukagroup@gmail.com>
2025-04-03 02:57:25 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,44 @@
-const CACHE_NAME="jekyll-pwa-v1";const urlsToCache = ["{{ site.baseurl }}/",{% for post in site.posts %}"{{ site.baseurl }}{{ post.url }}",{% endfor %}"{{ site.baseurl }}/assets/css/style.css","{{ site.baseurl }}/assets/js/main.js"];self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE_NAME).then(e=>e.addAll(urlsToCache)))}),self.addEventListener("fetch",t=>{t.respondWith(caches.match(t.request).then(e=>e||fetch(t.request)))}),self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(e=>Promise.all(e.map(e=>{if(e!==CACHE_NAME)return caches.delete(e)}))))});
+const CACHE_NAME = "jekyll-pwa-v1";
+
+// Dynamically generate URLs to cache using Jekyll
+const urlsToCache = [
+    "{{ site.baseurl }}/",  // Home Page
+    {% for post in site.posts %}
+    "{{ site.baseurl }}{{ post.url }}",
+    {% endfor %}
+    "{{ site.baseurl }}/assets/css/style.css",
+    "{{ site.baseurl }}/assets/js/main.js"
+];
+
+// Install Service Worker and Cache Files
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+// Fetch from cache first, then update from network
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
+
+// Activate & Remove Old Cache
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cache => {
+                    if (cache !== CACHE_NAME) {
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
+});
