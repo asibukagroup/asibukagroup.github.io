256c94f6afb0002f6eb009d636d9d59911136530
ASIBUKA Group <admin@asibuka.com>
2025-04-07 01:31:12 +0700

    part 2

--- _plugins/amp_generator.rb
+++ _plugins/amp_generator.rb
@@ -1,36 +1,19 @@
 # _plugins/amp_generator.rb
 
 module Jekyll
-    class AmpFile < Page
+    class AmpPage < Page
       def initialize(site, base, original, permalink, output_dir)
         @site = site
         @base = base
-        @dir = output_dir
+        @dir  = output_dir
         @name = "index.html"
         self.process(@name)
   
         self.content = original.content
         self.data = original.data.dup
-  
         self.data["layout"] = "amp"
         self.data["permalink"] = permalink
         self.data["canonical_url"] = original.url
-      end
-    end
-  
-    class AmpPost < Document
-      def initialize(site, original, permalink, output_dir)
-        super(original.path, { site: site, collection: site.posts })
-        self.content = original.content
-        self.data = original.data.dup
-  
-        self.data["layout"] = "amp"
-        self.data["permalink"] = permalink
-        self.data["canonical_url"] = original.url
-        @output = nil
-  
-        # Force output path
-        @relative_path = File.join(output_dir, "index.html")
       end
     end
   
@@ -47,7 +30,7 @@ module Jekyll
           amp_permalink = File.join((page.data["permalink"] || page.url).sub(/\/$/, ""), "amp", "/")
           output_dir = page.url == "/" ? "amp" : amp_permalink.sub(/^\//, "").sub(/index\.html$/, "")
   
-          site.pages << AmpFile.new(site, site.source, page, amp_permalink, output_dir)
+          site.pages << AmpPage.new(site, site.source, page, amp_permalink, output_dir)
         end
   
         # === POSTS ===
@@ -57,14 +40,7 @@ module Jekyll
           amp_permalink = File.join(post.url.sub(/\/$/, ""), "amp", "/")
           output_dir = amp_permalink.sub(/^\//, "").sub(/index\.html$/, "")
   
-          amp_post = post.dup
-          amp_post.data = post.data.dup
-          amp_post.content = post.content
-          amp_post.data["layout"] = "amp"
-          amp_post.data["permalink"] = amp_permalink
-          amp_post.data["canonical_url"] = post.url
-  
-          site.posts.docs << amp_post
+          site.pages << AmpPage.new(site, site.source, post, amp_permalink, output_dir)
         end
       end
     end
