40a0d2bfc1b7a447351efc896851528945f02114
ASIBUKA Group <admin@asibuka.com>
2025-04-07 01:27:10 +0700

    amp for posts

--- _plugins/amp_generator.rb
+++ _plugins/amp_generator.rb
@@ -2,34 +2,35 @@
 
 module Jekyll
     class AmpFile < Page
-      def initialize(site, base, original)
+      def initialize(site, base, original, permalink, output_dir)
         @site = site
         @base = base
-  
-        # Get original permalink or URL
-        original_permalink = original.data['permalink'] || original.url
-  
-        # Build AMP permalink
-        amp_permalink = File.join(original_permalink.sub(/\/$/, ''), 'amp', '/')
-  
-        # Determine output dir
-        if original.url == '/' || original.relative_path == 'index.md'
-          @dir = 'amp'
-        else
-          @dir = amp_permalink.sub(/^\//, '').sub(/index\.html$/, '')
-        end
-  
-        @name = 'index.html'
+        @dir = output_dir
+        @name = "index.html"
         self.process(@name)
   
-        # Copy content and front matter
         self.content = original.content
         self.data = original.data.dup
   
-        # Set AMP-specific front matter
-        self.data['layout'] = 'amp'
-        self.data['permalink'] = amp_permalink
-        self.data['canonical_url'] = original.url
+        self.data["layout"] = "amp"
+        self.data["permalink"] = permalink
+        self.data["canonical_url"] = original.url
+      end
+    end
+  
+    class AmpPost < Document
+      def initialize(site, original, permalink, output_dir)
+        super(original.path, { site: site, collection: site.posts })
+        self.content = original.content
+        self.data = original.data.dup
+  
+        self.data["layout"] = "amp"
+        self.data["permalink"] = permalink
+        self.data["canonical_url"] = original.url
+        @output = nil
+  
+        # Force output path
+        @relative_path = File.join(output_dir, "index.html")
       end
     end
   
@@ -38,11 +39,32 @@ module Jekyll
       priority :low
   
       def generate(site)
+        # === PAGES ===
         site.pages.each do |page|
-          next unless page.extname == '.md' || page.ext == '.md'
-          next if page.url.include?('/amp/')
+          next unless page.extname == ".md" || page.ext == ".md"
+          next if page.url.include?("/amp/")
   
-          site.pages << AmpFile.new(site, site.source, page)
+          amp_permalink = File.join((page.data["permalink"] || page.url).sub(/\/$/, ""), "amp", "/")
+          output_dir = page.url == "/" ? "amp" : amp_permalink.sub(/^\//, "").sub(/index\.html$/, "")
+  
+          site.pages << AmpFile.new(site, site.source, page, amp_permalink, output_dir)
+        end
+  
+        # === POSTS ===
+        site.posts.docs.each do |post|
+          next if post.url.include?("/amp/")
+  
+          amp_permalink = File.join(post.url.sub(/\/$/, ""), "amp", "/")
+          output_dir = amp_permalink.sub(/^\//, "").sub(/index\.html$/, "")
+  
+          amp_post = post.dup
+          amp_post.data = post.data.dup
+          amp_post.content = post.content
+          amp_post.data["layout"] = "amp"
+          amp_post.data["permalink"] = amp_permalink
+          amp_post.data["canonical_url"] = post.url
+  
+          site.posts.docs << amp_post
         end
       end
     end
