8811d859a1659000379e053208c521e55b4508fc
ASIBUKA Group <admin@asibuka.com>
2025-04-06 18:43:37 +0700

    minor update

--- _plugins/amp_generator.rb
+++ _plugins/amp_generator.rb
@@ -5,7 +5,6 @@ module Jekyll
         @base = base
         @dir  = File.join(original_page.url.sub(%r{^/}, ''), 'amp')
         @name = 'index.html'
-  
         self.process(@name)
         self.read_yaml(File.join(base, '_layouts'), 'amp.html')
         self.data = original_page.data.clone
@@ -15,26 +14,18 @@ module Jekyll
         self.content = output_html
       end
     end
-  
     class AMPGenerator < Generator
       safe true
       priority :lowest
-  
       def generate(site)
         markdown_converter = site.find_converter_instance(Jekyll::Converters::Markdown)
-  
-        # Generate AMP for all posts
         site.posts.docs.each do |post|
           post_output = post.output || markdown_converter.convert(post.content)
           site.pages << AMPPage.new(site, site.source, post, post_output)
         end
-  
-        # Generate AMP for pages (including homepage)
         site.pages.each do |page|
-          next if page.url.include?('/amp/') # skip already AMP pages
+          next if page.url.include?('/amp/')
           next if page.data['skip_amp'] == true
-  
-          # Render HTML content if needed
           page_output = if page.output
                           page.output
                         else
