3886fc4b7dc9a5dbe07e0001653f478596ed64ce
ASIBUKA Group <admin@asibuka.com>
2025-04-06 15:01:59 +0700

    amp 1.0.1

---
+++ _plugins/amp_generator.rb
@@ -0,0 +1,28 @@
+module Jekyll
+    class AMPPage < Page
+      def initialize(site, base, post)
+        @site = site
+        @base = base
+        @dir  = File.join(post.url.sub(%r{^/}, ''), 'amp')
+        @name = 'index.html'
+  
+        self.process(@name)
+        self.data = post.data.clone
+        self.data['layout'] = 'amp'
+        self.data['canonical_url'] = post.url
+        self.content = post.content
+      end
+    end
+  
+    class AMPGenerator < Generator
+      priority :low
+      safe true
+  
+      def generate(site)
+        site.posts.docs.each do |post|
+          site.pages << AMPPage.new(site, site.source, post)
+        end
+      end
+    end
+  end
+  
