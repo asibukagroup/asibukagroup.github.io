e994e34f344b7ec60dcd94f8f0d2cb68071f62cf
ASIBUKA Group <admin@asibuka.com>
2025-04-07 02:16:56 +0700

    part 4

--- _plugins/amp_generator.rb
+++ _plugins/amp_generator.rb
@@ -1,27 +1,29 @@
+# _plugins/amp_generator.rb
+
 require "nokogiri"
 
 module Jekyll
   class AmpPage < Page
-    def initialize(site, base, original, permalink, output_dir)
+    def initialize(site:, base:, original:, permalink:, output_dir:)
       @site = site
       @base = base
       @dir  = output_dir
       @name = "index.html"
-      self.process(@name)
+      process(@name)
 
-      # Render Markdown to HTML
-      renderer = site.find_converter_instance(Jekyll::Converters::Markdown)
-      rendered_html = renderer.convert(original.content)
-
-      # Convert to AMP-compliant HTML
-      amp_html = convert_to_amp(rendered_html)
-
-      # Set content and metadata
-      self.content = amp_html
+      # Handle front matter
       self.data = original.data.dup
       self.data["layout"] = "amp"
       self.data["permalink"] = permalink
       self.data["canonical_url"] = original.url
+
+      # Render Markdown to HTML using converter
+      markdown_converter = site.find_converter_instance(Jekyll::Converters::Markdown)
+      raw_content = original.content
+      rendered_html = markdown_converter.convert(raw_content)
+
+      # Apply AMP conversions
+      self.content = convert_to_amp(rendered_html)
     end
 
     private
@@ -30,29 +32,29 @@ module Jekyll
       doc = Nokogiri::HTML::DocumentFragment.parse(html)
 
       # Convert <img> to <amp-img>
-      doc.css('img').each do |img|
+      doc.css("img").each do |img|
         amp_img = Nokogiri::XML::Node.new("amp-img", doc)
         img.attributes.each { |name, attr| amp_img[name] = attr.value }
-        amp_img['layout'] ||= 'responsive'
-        amp_img['width'] ||= '600'
-        amp_img['height'] ||= '400'
+        amp_img["layout"] ||= "responsive"
+        amp_img["width"] ||= "600"
+        amp_img["height"] ||= "400"
         img.replace(amp_img)
       end
 
       # Convert <iframe> to <amp-iframe>
-      doc.css('iframe').each do |iframe|
+      doc.css("iframe").each do |iframe|
         amp_iframe = Nokogiri::XML::Node.new("amp-iframe", doc)
         iframe.attributes.each { |name, attr| amp_iframe[name] = attr.value }
-        amp_iframe['layout'] ||= 'responsive'
-        amp_iframe['sandbox'] ||= 'allow-scripts allow-same-origin'
-        amp_iframe['width'] ||= '600'
-        amp_iframe['height'] ||= '400'
+        amp_iframe["layout"] ||= "responsive"
+        amp_iframe["sandbox"] ||= "allow-scripts allow-same-origin"
+        amp_iframe["width"] ||= "600"
+        amp_iframe["height"] ||= "400"
         iframe.replace(amp_iframe)
       end
 
       # Remove non-AMP <script> tags
-      doc.css('script').each do |script|
-        script.remove unless script['src']&.include?('https://cdn.ampproject.org/')
+      doc.css("script").each do |script|
+        script.remove unless script["src"]&.include?("https://cdn.ampproject.org/")
       end
 
       doc.to_html
@@ -64,25 +66,37 @@ module Jekyll
     priority :low
 
     def generate(site)
-      # === PAGES ===
+      # === Generate AMP for Pages ===
       site.pages.each do |page|
         next unless page.extname == ".md" || page.ext == ".md"
         next if page.url.include?("/amp/")
 
         amp_permalink = File.join((page.data["permalink"] || page.url).sub(/\/$/, ""), "amp", "/")
-        output_dir = page.url == "/" ? "amp" : amp_permalink.sub(/^\//, "").sub(/index\.html$/, "")
+        output_dir = page.url == "/" ? "amp" : amp_permalink.sub(/^\//, "").chomp("/")
 
-        site.pages << AmpPage.new(site, site.source, page, amp_permalink, output_dir)
+        site.pages << AmpPage.new(
+          site: site,
+          base: site.source,
+          original: page,
+          permalink: amp_permalink,
+          output_dir: output_dir
+        )
       end
 
-      # === POSTS ===
+      # === Generate AMP for Posts ===
       site.posts.docs.each do |post|
         next if post.url.include?("/amp/")
 
         amp_permalink = File.join(post.url.sub(/\/$/, ""), "amp", "/")
-        output_dir = amp_permalink.sub(/^\//, "").sub(/index\.html$/, "")
+        output_dir = amp_permalink.sub(/^\//, "").chomp("/")
 
-        site.pages << AmpPage.new(site, site.source, post, amp_permalink, output_dir)
+        site.pages << AmpPage.new(
+          site: site,
+          base: site.source,
+          original: post,
+          permalink: amp_permalink,
+          output_dir: output_dir
+        )
       end
     end
   end
