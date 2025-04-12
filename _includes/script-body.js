2dc98c5c45c6445ffe0616a9ba1c678a2483d06d
ASIBUKA Group <admin@asibuka.com>
2025-04-13 03:02:55 +0700

    create random posts function

--- _includes/script-body.js
+++ _includes/script-body.js
@@ -2,6 +2,8 @@
 const body=document.body,darkToggle=document.getElementById("darkToggle");"enabled"===localStorage.getItem("dark-mode")&&body.classList.add("dark"),darkToggle.addEventListener("click",()=>{body.classList.toggle("dark"),localStorage.setItem("dark-mode",body.classList.contains("dark")?"enabled":"disabled")});
 // PWA
 "serviceWorker"in navigator&&navigator.serviceWorker.register("/sw.js").then(e=>console.log("Service Worker Registered!",e)).catch(e=>console.log("Service Worker Registration Failed!",e));
+// Random Posts
+fetch('{{ "/search.json" | relative_url }}').then(n=>n.json()).then(n=>{0!==n.length&&(n=n[Math.floor(Math.random()*n.length)],window.randomLink=n.url)}).catch(console.error);
 // Lazyload Scripts
 function lazyLoadScript(n,c,e){var i=!1;function t(){if(!i){i=!0;var e,t=document.createElement("script");for(e in t.src=n,t.async=!0,c)c.hasOwnProperty(e)&&t.setAttribute(e,c[e]);document.head.appendChild(t)}}c=c||{},"scroll"===(e=e||"click")?window.addEventListener("scroll",t,{once:!0}):"timeout"===e?setTimeout(t,3e3):(window.addEventListener("click",t,{once:!0}),window.addEventListener("touchstart",t,{once:!0}))}
 // Lazyload AdSense
