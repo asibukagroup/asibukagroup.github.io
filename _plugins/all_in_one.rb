655fd4d5cf071ce404babb77c3c238d72128edbc
ASIBUKA Group <admin@asibuka.com>
2025-04-16 10:07:09 +0700

    part 13

--- _plugins/all_in_one.rb
+++ _plugins/all_in_one.rb
@@ -6,6 +6,7 @@ module Jekyll
     def generate(site)
       collections = site.config['collections'].keys
 
+      # Process collection documents
       site.collections.each_value do |collection|
         collection.docs.each do |doc|
           next unless valid_md_doc?(doc, collections)
@@ -15,6 +16,7 @@ module Jekyll
         end
       end
 
+      # Process standalone .md files in root
       site.pages.each do |page|
         next unless valid_md_page?(page)
         next if page.data['is_amp']
@@ -29,12 +31,9 @@ module Jekyll
       doc.path.end_with?('.md') && collections.any? { |c| doc.path.include?("_#{c}/") }
     end
 
+    # Match any .md file in the root directory
     def valid_md_page?(page)
-      page.path.end_with?('.md') &&
-        (
-          File.basename(page.path) == 'index.md' || # ensure homepage is included
-          File.dirname(page.path) == '.' || File.dirname(page.path) == ''
-        )
+      page.path.end_with?('.md') && File.dirname(page.path) == '.'
     end
 
     def generate_amp_page(site, original)
@@ -43,7 +42,7 @@ module Jekyll
       amp_data['layout'] ||= 'amp'
       amp_data['permalink'] = original.url.sub(/\/$/, '') + '/amp/'
 
-      filename = original.basename_without_ext + '-amp.md'
+      filename = File.basename(original.path, File.extname(original.path)) + '-amp.md'
       dir = File.dirname(original.path.sub(site.source, ''))
 
       amp_page = PageWithoutAFile.new(site, site.source, dir, filename)
