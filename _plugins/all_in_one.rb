23bd38e8e087dce8a17733f28491e7b5f3241214
ASIBUKA Group <admin@asibuka.com>
2025-04-16 10:17:58 +0700

    add function to create amp version for pages from jekyll-archive

--- _plugins/all_in_one.rb
+++ _plugins/all_in_one.rb
@@ -6,22 +6,26 @@ module Jekyll
     def generate(site)
       collections = site.config['collections'].keys
 
-      # Process collection docs (e.g., posts, products)
+      # Process collection documents (e.g., posts)
       site.collections.each_value do |collection|
         collection.docs.each do |doc|
           next unless valid_md_doc?(doc, collections)
           next if doc.data['is_amp']
-
           site.pages << generate_amp_page(site, doc)
         end
       end
 
-      # Process .md files in root directory
+      # Process .md files in the root
       site.pages.each do |page|
         next unless valid_md_page?(page)
         next if page.data['is_amp']
-
         site.pages << generate_amp_page(site, page)
+      end
+
+      # Process archive pages from jekyll-archives
+      site.pages.each do |page|
+        next unless archive_page?(page)
+        site.pages << generate_amp_archive(site, page)
       end
     end
 
@@ -35,10 +39,13 @@ module Jekyll
       page.path.end_with?('.md') && File.dirname(page.path) == '.'
     end
 
+    def archive_page?(page)
+      page.data['layout'] == 'archive' && !page.data['is_amp']
+    end
+
     def generate_amp_page(site, original)
       amp_data = original.data.dup
       amp_data['is_amp'] = true
-      amp_data['layout'] ||= 'amp'
       amp_data['permalink'] = original.url.sub(/\/$/, '') + '/amp/'
 
       basename = File.basename(original.path, File.extname(original.path))
@@ -47,6 +54,18 @@ module Jekyll
 
       amp_page = PageWithoutAFile.new(site, site.source, amp_dir, amp_filename)
       amp_page.content = original.content
+      amp_page.data = amp_data
+
+      amp_page
+    end
+
+    def generate_amp_archive(site, page)
+      amp_data = page.data.dup
+      amp_data['is_amp'] = true
+      amp_data['permalink'] = page.url.sub(/\/$/, '') + '/amp/'
+
+      amp_page = PageWithoutAFile.new(site, site.source, page.dir, 'index-amp.html')
+      amp_page.content = page.content
       amp_page.data = amp_data
 
       amp_page
