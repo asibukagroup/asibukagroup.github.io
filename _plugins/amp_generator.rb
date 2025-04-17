dadeaf53d6428beda5416ea38f7526352a5a5786
ASIBUKA Group <admin@asibuka.com>
2025-04-17 12:05:10 +0700

    update the unknown result of liquid

--- _plugins/amp_generator.rb
+++ _plugins/amp_generator.rb
@@ -49,17 +49,18 @@ module Jekyll
       amp_data = original.data.dup
       amp_data['is_amp'] = true
       amp_data['permalink'] = original.url.sub(/\/$/, '') + '/amp/'
-
+    
       basename = File.basename(original.path, File.extname(original.path))
       amp_filename = "#{basename}-amp.md"
       amp_dir = File.dirname(original.path.sub(site.source, ''))
-
+    
       amp_page = PageWithoutAFile.new(site, site.source, amp_dir, amp_filename)
-      amp_page.content = convert_html_for_amp(original.content)
+      amp_page.content = original.content
       amp_page.data = amp_data
-
+    
       amp_page
     end
+    
 
     def duplicate_archive_as_amp(site, original)
       amp_data = original.data.dup
